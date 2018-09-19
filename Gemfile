62aef4a894165db122a25c146ba9b00517fb97bb
Romain Chartier <romain.chartier@cegedim.com>
2018-09-19 23:00:17 +0200

    Init

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+# github theme
+gem "github-pages", group: :jekyll_plugins
+
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export JEKYLL_VERSION=3.8
+docker run --rm \
+  --volume=$PWD:/srv/jekyll \
+  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+  -p 35729:35729 -p 4000:4000 \
+  -it jekyll/builder:$JEKYLL_VERSION \
+  jekyll $@
